9d2861c01d742267984799fb12bf38e6202a43fe
Philip Jurke <pjurke@icloud.com>
2025-11-11 14:28:28 +0100

    integrate new cuid id

--- lib/zod/extensions.ts
+++ lib/zod/extensions.ts
@@ -1,9 +1,6 @@
 import { z } from "zod";
 
-const objectIdRegex = /^[0-9a-fA-F]{24}$/;
-const defaultOidMessage = 'Must be a valid ObjectID.';
+const defaultCuidMessage = 'Must be a valid CUID';
 
-export const oid = (message?: string) =>
-  z.string()
-    .regex(objectIdRegex, { message: message ?? defaultOidMessage })
-    .transform((v) => v.toLowerCase());
+export const cuid = (message?: string) =>
+  z.cuid2({ message: message ?? defaultCuidMessage });
